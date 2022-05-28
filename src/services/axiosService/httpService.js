a7a821f519751b092a5b83551a65946a6614b25f
pamudu-ratnayake <it19993416@my.sliit.lk>
2022-05-28 23:44:32 +0530

    httpServices added

---
+++ src/services/axiosService/httpService.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+import API from "../../variables/tokenURL";
+
+function httpService() {
+
+  function getAxios(url) {
+    return API({
+      url: url,
+      method: "GET",
+    //   params: params,
+    });
+  }
+
+  function deleteAxios(url) {
+    return API({
+      url: url,
+      method: "DELETE",
+    //   params: params,
+    });
+  }
+
+  function postAxios(url,values) {
+    return API({
+      url: url,
+      method: "POST",
+      // params: params,
+      // body: body,
+      data: values,
+    //   headers: {"Access-Control-Allow-Origin": "*"}
+    //   headers: 'Access-Control-Allow-Origin:http://localhost:8080'
+    });
+  }
+
+  function putAxios(url,values) {
+    return API({
+      url: url,
+      method: "PUT",
+      // params: params,
+      // body: body,
+      data: values,
+    });
+  }
+
+  return {
+    getAxios,
+    deleteAxios,
+    postAxios,
+    putAxios,
+
+  };
+}
+
+export default httpService();
